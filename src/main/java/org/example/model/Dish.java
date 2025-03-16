ac3e5cf1cd11b72d4be37d56bb4c0c2dd0ead883
lorransantosdev <lorransantossarmentol@gmail.com>
2025-03-16 18:26:19 -0300

    classes models criadas

---
+++ src/main/java/org/example/model/Dish.java
@@ -0,0 +1,26 @@
+package org.example.model;
+
+
+public class Dish {
+
+    private String name;
+    private double price;
+    private String description;
+
+    public Dish(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+
+    public double getPrice() { return price; }
+
+    public String getDescription() { return description; }
+
+    @Override
+    public String toString() {
+        return name + " - $" + price + " - " + description;
+    }
+}
---
+++ src/main/java/org/example/model/Order.java
@@ -0,0 +1,38 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private int orderNumber;
+    private String customer;
+    private List<Dish> dishList;
+    private double total;
+
+    public Order(int orderNumber, String customer) {
+        this.orderNumber = orderNumber;
+        this.customer = customer;
+        this.dishList = new ArrayList<>();
+        this.total = 0.0;
+    }
+
+    public void addDish(Dish dish) {
+        dishList.add(dish);
+        total += dish.getPrice();
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void viewOrder() {
+        System.out.println("Order Number: " + orderNumber);
+        System.out.println("Customer: " + customer);
+        System.out.println("Dishes:");
+        for (Dish dish : dishList) {
+            System.out.println(dish);
+        }
+        System.out.println("Total: $" + total);
+    }
+}
+
